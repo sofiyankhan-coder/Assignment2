018be198d4c9062dfa4e9f8e0818c4fa29c3825e
Sofiyan Khan <sofiyankhan-coder>
2025-11-07 22:36:27 -0500

    commit 6

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
