5409c736b9aa7f5a53eb818f84ddf2cac1b3507b
Sofiyan Khan <sofiyankhan-coder>
2025-11-07 21:19:19 -0500

    commit 4

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
+
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
